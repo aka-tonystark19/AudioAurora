a813ed2661aa7b3aef677371539f79339ed77014
Aditya Phalod <adityaphalod@gmail.com>
2023-04-06 17:14:12 -0700

    more wifi

--- wifi/audioUart/software/aUart/hello_world_small.c
+++ wifi/audioUart/software/aUart/hello_world_small.c
@@ -1,267 +1,308 @@
-///*
-// * "Small Hello World" example.
-// *
-// * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
-// * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
-// * designs. It requires a STDOUT  device in your system's hardware.
-// *
-// * The purpose of this example is to demonstrate the smallest possible Hello
-// * World application, using the Nios II HAL library.  The memory footprint
-// * of this hosted application is ~332 bytes by default using the standard
-// * reference design.  For a more fully featured Hello World application
-// * example, see the example titled "Hello World".
-// *
-// * The memory footprint of this example has been reduced by making the
-// * following changes to the normal "Hello World" example.
-// * Check in the Nios II Software Developers Manual for a more complete
-// * description.
-// *
-// * In the SW Application project (small_hello_world):
-// *
-// *  - In the C/C++ Build page
-// *
-// *    - Set the Optimization Level to -Os
-// *
-// * In System Library project (small_hello_world_syslib):
-// *  - In the C/C++ Build page
-// *
-// *    - Set the Optimization Level to -Os
-// *
-// *    - Define the preprocessor option ALT_NO_INSTRUCTION_EMULATION
-// *      This removes software exception handling, which means that you cannot
-// *      run code compiled for Nios II cpu with a hardware multiplier on a core
-// *      without a the multiply unit. Check the Nios II Software Developers
-// *      Manual for more details.
-// *
-// *  - In the System Library page:
-// *    - Set Periodic system timer and Timestamp timer to none
-// *      This prevents the automatic inclusion of the timer driver.
-// *
-// *    - Set Max file descriptors to 4
-// *      This reduces the size of the file handle pool.
-// *
-// *    - Check Main function does not exit
-// *    - Uncheck Clean exit (flush buffers)
-// *      This removes the unneeded call to exit when main returns, since it
-// *      won't.
-// *
-// *    - Check Don't use C++
-// *      This builds without the C++ support code.
-// *
-// *    - Check Small C library
-// *      This uses a reduced functionality C library, which lacks
-// *      support for buffering, file IO, floating point and getch(), etc.
-// *      Check the Nios II Software Developers Manual for a complete list.
-// *
-// *    - Check Reduced device drivers
-// *      This uses reduced functionality drivers if they're available. For the
-// *      standard design this means you get polled UART and JTAG UART drivers,
-// *      no support for the LCD driver and you lose the ability to program
-// *      CFI compliant flash devices.
-// *
-// *    - Check Access device drivers directly
-// *      This bypasses the device file system to access device drivers directly.
-// *      This eliminates the space required for the device file system services.
-// *      It also provides a HAL version of libc services that access the drivers
-// *      directly, further reducing space. Only a limited number of libc
-// *      functions are available in this configuration.
-// *
-// *    - Use ALT versions of stdio routines:
-// *
-// *           Function                  Description
-// *        ===============  =====================================
-// *        alt_printf       Only supports %s, %x, and %c ( < 1 Kbyte)
-// *        alt_putstr       Smaller overhead than puts with direct drivers
-// *                         Note this function doesn't add a newline.
-// *        alt_putchar      Smaller overhead than putchar with direct drivers
-// *        alt_getchar      Smaller overhead than getchar with direct drivers
-// *
-// */
-//
+//#include <stdio.h>
+//#include "system.h"
 //#include "sys/alt_stdio.h"
+//#include "altera_avalon_pio_regs.h"
+//#include "altera_avalon_uart_regs.h"
+//#include <stdint.h>
 //
-//int main()
-//{
-//  alt_putstr("Hello from Nios II!\n");
+//enum State {WAIT, FIRST, SECOND, END};
 //
-//  /* Event loop never exits. */
-//  while (1);
+//int main(void) {
+//    alt_putstr("Hello from Nios II!\n");
+//	char input;
 //
-//  return 0;
+//    char *ptr = ONCHIP_MEMORY2_0_BASE;
+//
+//    // create a state variable
+//    int i = 0;
+//    uint8_t curr = 0;
+//    uint8_t temp = 0;
+//    enum State state = WAIT;
+//
+//    while (state != END) {
+//		input = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//        switch (state) {
+//            case WAIT:
+//                if (input == '!') {
+//                	printf("Chunk Start\n");
+//                    state = FIRST;
+//                }
+//                break;
+//            case FIRST:
+//                if (input == '?') {
+//                	printf("WAIT %d\n", i++);
+//                    state = WAIT;
+//                } else if (input == '~') {
+//                    state = END;
+//                } else {
+//                    curr = 0;
+//                    switch(input) {
+//                        case '0':   temp = 0;   break;
+//                        case '1':   temp = 1;   break;
+//                        case '2':   temp = 2;   break;
+//                        case '3':   temp = 3;   break;
+//                        case '4':   temp = 4;   break;
+//                        case '5':   temp = 5;   break;
+//                        case '6':   temp = 6;   break;
+//                        case '7':   temp = 7;   break;
+//                        case '8':   temp = 8;   break;
+//                        case '9':   temp = 9;   break;
+//                        case 'A':   temp = 10;  break;
+//                        case 'B':   temp = 11;  break;
+//                        case 'C':   temp = 12;  break;
+//                        case 'D':   temp = 13;  break;
+//                        case 'E':   temp = 14;  break;
+//                        case 'F':   temp = 15;  break;
+//                        default: printf("1 BRUH %d\n", input);
+//                    }
+//
+//                    curr = temp * 16;
+//                    state = SECOND;
+//                }
+//                break;
+//            case SECOND:
+//            	if (input == '?') {
+//            		printf("WAIT %d\n", i++);
+//            		state = WAIT;
+//            	} else if (input == '~') {
+//            		state = END;
+//            	} else {
+//            		switch(input) {
+//            			case '0':   temp = 0;   break;
+//            			case '1':   temp = 1;   break;
+//            			case '2':   temp = 2;   break;
+//            			case '3':   temp = 3;   break;
+//            			case '4':   temp = 4;   break;
+//            			case '5':   temp = 5;   break;
+//            			case '6':   temp = 6;   break;
+//            			case '7':   temp = 7;   break;
+//            			case '8':   temp = 8;   break;
+//            			case '9':   temp = 9;   break;
+//            			case 'A':   temp = 10;  break;
+//            			case 'B':   temp = 11;  break;
+//            			case 'C':   temp = 12;  break;
+//            			case 'D':   temp = 13;  break;
+//            			case 'E':   temp = 14;  break;
+//            			case 'F':   temp = 15;  break;
+//            			default: printf("2 BRUH %d\n", input);
+//            		}
+//
+//            		curr += temp;
+//            		*ptr++ = curr;
+//
+//            		state = FIRST;
+//            	}
+//
+//                break;
+//            case END:
+//                break;
+//        }
+//    }
+//
+//    printf("done!\n");
+//
+//    char *read = ONCHIP_MEMORY2_0_BASE;
+//
+//    while (read != ptr) {
+//    	printf("%c", *read++);
+//    }
+//
 //}
 
 
-/* 
- * "Small Hello World" example. 
- * 
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example 
- * designs. It requires a STDOUT  device in your system's hardware. 
- *
- * The purpose of this example is to demonstrate the smallest possible Hello 
- * World application, using the Nios II HAL library.  The memory footprint
- * of this hosted application is ~332 bytes by default using the standard 
- * reference design.  For a more fully featured Hello World application
- * example, see the example titled "Hello World".
- *
- * The memory footprint of this example has been reduced by making the
- * following changes to the normal "Hello World" example.
- * Check in the Nios II Software Developers Manual for a more complete 
- * description.
- * 
- * In the SW Application project (small_hello_world):
- *
- *  - In the C/C++ Build page
- * 
- *    - Set the Optimization Level to -Os
- * 
- * In System Library project (small_hello_world_syslib):
- *  - In the C/C++ Build page
- * 
- *    - Set the Optimization Level to -Os
- * 
- *    - Define the preprocessor option ALT_NO_INSTRUCTION_EMULATION 
- *      This removes software exception handling, which means that you cannot 
- *      run code compiled for Nios II cpu with a hardware multiplier on a core 
- *      without a the multiply unit. Check the Nios II Software Developers 
- *      Manual for more details.
- *
- *  - In the System Library page:
- *    - Set Periodic system timer and Timestamp timer to none
- *      This prevents the automatic inclusion of the timer driver.
- *
- *    - Set Max file descriptors to 4
- *      This reduces the size of the file handle pool.
- *
- *    - Check Main function does not exit
- *    - Uncheck Clean exit (flush buffers)
- *      This removes the unneeded call to exit when main returns, since it
- *      won't.
- *
- *    - Check Don't use C++
- *      This builds without the C++ support code.
- *
- *    - Check Small C library
- *      This uses a reduced functionality C library, which lacks  
- *      support for buffering, file IO, floating point and getch(), etc. 
- *      Check the Nios II Software Developers Manual for a complete list.
- *
- *    - Check Reduced device drivers
- *      This uses reduced functionality drivers if they're available. For the
- *      standard design this means you get polled UART and JTAG UART drivers,
- *      no support for the LCD driver and you lose the ability to program 
- *      CFI compliant flash devices.
- *
- *    - Check Access device drivers directly
- *      This bypasses the device file system to access device drivers directly.
- *      This eliminates the space required for the device file system services.
- *      It also provides a HAL version of libc services that access the drivers
- *      directly, further reducing space. Only a limited number of libc
- *      functions are available in this configuration.
- *
- *    - Use ALT versions of stdio routines:
- *
- *           Function                  Description
- *        ===============  =====================================
- *        alt_printf       Only supports %s, %x, and %c ( < 1 Kbyte)
- *        alt_putstr       Smaller overhead than puts with direct drivers
- *                         Note this function doesn't add a newline.
- *        alt_putchar      Smaller overhead than putchar with direct drivers
- *        alt_getchar      Smaller overhead than getchar with direct drivers
- *
- */
-
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include "system.h"
 #include "sys/alt_stdio.h"
 #include "altera_avalon_pio_regs.h"
 #include "altera_avalon_uart_regs.h"
+#include "altera_avalon_uart.h"
+#include "altera_up_avalon_rs232.h"
+#include "altera_up_avalon_rs232_regs.h"
+
+#define DATA_SIZE 2000000
 
 void delay(int a){
 	volatile int i=0;
-	while(i<a*1000000){
+	while(i<a*1000){
 		i++;
 	}
 }
 
-void ledDemo(){
-	char a;
-	int b;
-	int led_on = 0;
-	a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
-	b = (int)a-48;
-	led_on ^= (1<<(b-1));
-	IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE,led_on);
-	printf("Led position %d\n", a);
-}
+int main(void){
+//	printf("Ready to Receive Text\n");
+//	volatile int* data = IOADDR_ALT_UP_RS232_DATA(RS232_BASE);
+//	char* msg = "!1234567890?";
+//	char buf[20] = {0};
+//	alt_putstr("Test 1: Sending: "); alt_putstr(msg); alt_putstr("\n");
+//	for (int i = 0; i < strlen(msg); i++) {
+//		*data = (msg[i] >> ALT_UP_RS232_DATA_DATA_OFST) & ALT_UP_RS232_DATA_DATA_MSK;
+//		delay(1);
+//		buf[i] = (*data & ALT_UP_RS232_DATA_DATA_MSK) >> ALT_UP_RS232_DATA_DATA_OFST;
+//		printf("%c", buf[i]);
+//	}
+//	printf("\n");
+//	alt_putstr("Check 1: buffer: "); alt_putstr(buf); alt_putstr("\n");
 
-#define DATA_SIZE 2000000
+	printf("Ready to Receive Text\n");
+	volatile int* data = IOADDR_ALT_UP_RS232_DATA(RS232_BASE);
+	char buf[100] = {0};
+	char a = '#';
 
-int main()
-{ 
-  alt_putstr("Hello from Nios II!\n");
+	char holder = (*data & ALT_UP_RS232_DATA_DATA_MSK) >> ALT_UP_RS232_DATA_DATA_OFST;
 
-  /* Event loop never exits. */
-//	  printf("Hello from UART\n");
-	  char holder = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
-	  char temp = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
-	  char fileData[1000];
-	  unsigned int i = 0;
-//	  while(1) {
-//		  printf("%d\n", IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE));
-//	  }
-	  while(1){
+//	while(1){
+//			a = (*data & ALT_UP_RS232_DATA_DATA_MSK) >> ALT_UP_RS232_DATA_DATA_OFST;
+//			if (a == '!'){
+//				while (1){
+//					a = (*data & ALT_UP_RS232_DATA_DATA_MSK) >> ALT_UP_RS232_DATA_DATA_OFST;
+//					if (a == '?' || a == '~' || a == '!'){
+//						printf("\n");
+//						break;
+//					}
+//					else {
+//						printf("%c", a);
+//						delay(1);
+//					}
+//				}
+//			}
+//			if (a == '~') break;
+//		}
 
-		  char a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
-		  // printf("%c\n", a);
-		  if(i>=1000){
-			  printf("%s",fileData);
-			  break;
-			  i=0;
-		  }
-		  if(a!='\''){
-		  fileData[i]=a;
-		  i=i+1;
-		  }
-//		  if (a != NULL) {
-//			  fileData[i] = a;
-//////			  printf("New Data: %c\n",fileData[i]);
-////			  printf("New Data: %c\n",a);
-//		  }
-//		  i++;
-//		  if (i == DATA_SIZE - 1) {
-//			  i = 0;
-//		  }
-//		  else {
-//			  i++;
-//		  }
-//		  if(a!=holder){
-//			  if(a!=NULL){
-//				  printf("%c\n",a);
+		while(1){
+				a = (*data & ALT_UP_RS232_DATA_DATA_MSK) >> ALT_UP_RS232_DATA_DATA_OFST;
+				if (a == '!'){
+					while (1){
+						a = (*data & ALT_UP_RS232_DATA_DATA_MSK) >> ALT_UP_RS232_DATA_DATA_OFST;
+						if (a == '?' || a == '~' || a == '!'){
+							printf("\n");
+							break;
+						}
+						else {
+							printf("%c", a);
+							delay(1);
+						}
+					}
+				}
+				if (a == '~') break;
+			}
+
+
+//	while(1){
 //
-//			  holder=a;
-//		  }
-//	  }
-	 }
+//
+//			  char a = (*data & ALT_UP_RS232_DATA_DATA_MSK) >> ALT_UP_RS232_DATA_DATA_OFST;
+//			  if(a!=holder){
+//				printf("%c",a);
+//			  	holder=a;
+//			  }
+//
+//			return 0;
+//	}
 
-//	  for(int i=0;i<1000;i++){
-//		  printf("%c", fileData[i]);
-//	  }
-	  //ledDemo();
-//	  delay(1);
-
-//  alt_u8 status;
-//  status= IORD_ALTERA_AVALON_UART_STATUS(RS232_BASE);
-////  char holder = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
-//  while(1){
-//	  status = IORD_ALTERA_AVALON_UART_STATUS(RS232_BASE);
-//	  if(status == 234){
-//		  char a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
-//		  printf("Status: %d, Character: %c\n",status, a);
-////		  holder=a;
-//	  }
-//  }
+	printf("Packet Recieved!\n");
+	while(1);
 
 
-  return 0;
+//	for (uint8_t i = 0; i < 100; i++) {
+//		IOWR_ALTERA_AVALON_UART_RXDATA(RS232_BASE, 35);
+//		char a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//		printf("%d\n", a);
+//	}
+
+//	char a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//	while(1){
+//		a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//		if (a == '!'){
+//			while (1){
+//				a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//				if (a == '?' || a == '~'){
+//					printf("\n");
+//					break;
+//				}
+//				else {
+//					printf("%c", a);
+//				}
+//			}
+//		}
+//	}
 }
+
+//int main(){
+//  alt_putstr("Hello from Nios II!\n");
+//
+//  /* Event loop never exits. */
+////	  printf("Hello from UART\n");
+//	  char holder = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//	  char temp = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//	  char fileData[10000];
+//	  unsigned int i = 0;
+////	  while(1) {
+////		  printf("%d\n", IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE));
+////	  }
+//	  while(1){
+//
+//		  char a = IORD_ALTERA_AVALON_UART_RXDATA(RS232_BASE);
+//		  if(a!=holder){
+//			i++;
+//			if (i%2==1)	printf("%c",a);
+////			if (i%2==0)	printf("%c",a);
+//		  	holder=a;
+//		  }
+////		  // printf("%c\n", a);
+////		  if(a!='q'){
+////			  fileData[i]=a;
+////			  i=i+1;
+////		  }
+////
+////		  if (i >= 1000) break;
+//	  }
+//
+////	  printf("%s", fileData);
+//
+//		  return 0;
+//}
+
+
+// Another Trial
+
+//#include <stdio.h>
+//#include "altera_up_avalon_uart.h"
+//
+//#define BAUD_RATE 115200      // Desired baud rate
+//
+//int main()
+//{
+//
+////	alt_up_uart_dev *uart = alt_up_uart_open_dev(RS232_BASE); // Open the UART device
+//
+//
+//	alt_up_rs232_dev *uart = alt_up_rs232_open_dev(RS232_BASE);
+//
+////	alt_up_uart_init(uart); // Initialize the UART module
+//
+//	ALTERA_UP_AVALON_RS232_INIT(uart,);
+//
+//	alt_up_uart_set_config(uart, BAUD_RATE, 8, 'N', 1); // Configure the UART module
+//
+//    printf("Waiting for start character '!'...\n");
+//
+//    unsigned char data;
+//    while (1) {
+//        alt_up_uart_read_data(uart, &data, 1); // Read one byte of data from the UART module
+//        if (data == '!') { // Start reading and printing when '!' is received
+//            printf("Start character '!' received. Reading and printing data...\n");
+//            while (1) {
+//                alt_up_uart_read_data(uart, &data, 1); // Read one byte of data from the UART module
+//                printf("%c", data); // Print the received data
+//                if (data == '?') { // Stop reading and printing when '?' is received
+//                    printf("\n");
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//
+//    return 0;
+//}
